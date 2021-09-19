5088dc9db252cd265535c172fd53400aea392f40
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-19 14:08:50 -0300

    update news.py

--- notenews/plugins/news.py
+++ notenews/plugins/news.py
@@ -28,12 +28,7 @@ def check_send():
 ▫️ | {entry.link}
 
 ◾️ | <code>Mantido por:</code> @NoteZV
-""" if not entry.feedburner_origlink else f"""
-🎮 {entry.title}
-▫️ | {entry.feedburner_origlink}
-
-◾️ | <code>Mantido por:</code> @NoteZV
-""" 
+"""
         try:
             NoteNews.send_message(Config.LOG_CHANNEL, message)
             db.update_link(feed_url, entry.id)
