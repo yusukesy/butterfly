eda07ce35197e061283f586e3a6d6af10275c21a
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-19 14:00:19 -0300

    update news.py

--- notenews/plugins/news.py
+++ notenews/plugins/news.py
@@ -19,20 +19,14 @@ def check_send():
     feed_url = random.choice(Config.FEED_URLS)
     FEED = feedparser.parse(feed_url)
     entry = FEED.entries[0]
+    link = entry.link if not entry.feedburner_origlink else entry.feedburner_origlink
     if db.get_link(feed_url) == None:
         db.update_link(feed_url, "*")
         return
     if entry.id != db.get_link(feed_url).link:
         message = f"""
 🎮 {entry.title}
-▫️ | {entry.link}
-
-◾️ | <code>Mantido por:</code> @NoteZV
-""" if not entry.feedburner_origlink else f"""
-
-🎮 {entry.title}
-
-▫️ | {entry.feedburner_origlink}
+▫️ | {link}
 
 ◾️ | <code>Mantido por:</code> @NoteZV
 """
