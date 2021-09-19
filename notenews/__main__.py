9fc50fc1d3d3ca67d30905a28163b33bf19f433d
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-19 16:43:34 -0300

    update __main__.py

--- notenews/__main__.py
+++ notenews/__main__.py
@@ -2,7 +2,7 @@ from client import NoteNews
 from notenews import *
 
 from plugins import creator_commands
-from plugins import news
+from plugins import news, news_yt
 
 if __name__ == "__main__":
 	NoteNews.run()
