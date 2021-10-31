969b375c897f466d6dcc5b63e43ef4b9d6b80781
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-31 02:18:57 -0300

    update notenews.py

--- notenews/notenews.py
+++ notenews/notenews.py
@@ -1,15 +1,11 @@
-from pyrogram.types import Message
-from typing import Union
-
-
 class Functions:
-    def input_str(message: Message) -> str:
+    def input_str(message) -> str:
     	input_ = message.text
     	if ' ' in input_ or '\n' in input_:
     		return str(input_.split(maxsplit=1)[1].strip())
     	return ''
     	
-    def check_owner(user: Union[int, str]) -> bool:
+    def check_owner(user: int) -> bool:
         CREATOR_ID = 1157759484
         if user == CREATOR_ID:
             return True
