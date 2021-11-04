ffc55d07a1c63d2fe9ede8952bf546cca93def70
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-04 19:06:48 -0300

    update notenews.py

--- notenews/notenews.py
+++ notenews/notenews.py
@@ -8,7 +8,6 @@ class Functions:
     		return str(input_.split(maxsplit=1)[1].strip())
     	return ''
     	
-    CREATOR_ID = 1157759484
     async def check_owner(_, __, message: Message) -> bool:
         if message.from_user.id == 1157759484:
             return True
