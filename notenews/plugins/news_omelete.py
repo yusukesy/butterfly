a037d4375f7c9dffda6f27a44cd5b37c4841e8ad
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-08 16:16:29 -0300

    update news_omelete.py

--- notenews/plugins/news_omelete.py
+++ notenews/plugins/news_omelete.py
@@ -38,5 +38,5 @@ def check_send():
             print(f"FEED Verificado: {link}")
             
 scheduler = BackgroundScheduler()
-scheduler.add_job(check_send, "interval", seconds=1, max_instances=Config.MAX_INSTANCES)
+scheduler.add_job(check_send, "interval", seconds=Config.CHECK_INTERVAL, max_instances=Config.MAX_INSTANCES)
 scheduler.start()
