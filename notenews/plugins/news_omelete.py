2d08486045eb119f29fb53df3d1168e854fcdc96
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-08 16:05:28 -0300

    update news_omelete.py

---
+++ notenews/plugins/news_omelete.py
@@ -0,0 +1,44 @@
+import bs4
+import requests
+
+from time import sleep
+
+from .sql import db
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from pyrogram.errors import FloodWait
+from client import Config, NoteNews
+
+
+title = str(soup.main.a.h2).replace("<h2>", "").replace("</h2>", "")
+link = "https://omelete.com.br" + soup.main.a.get("href")
+
+def check_send():
+    html = requests.get("https://www.omelete.com.br/noticias").content
+    soup = bs(html, "html.parser")
+    link = "https://omelete.com.br" + soup.main.a.get("href")
+    website = "https://omelete.com.br/noticias"
+    if link is not None:
+        if db.get_link(website) == None:
+            db.update_link(website, "*")
+            return
+        if link != db.get_link(website).link:
+            message = f"""
+[\u200c]({link})🌐 | via **Omelete:** **[{title}]({link})**
+
+▫️ | Mantido por: @NoteZV
+"""
+            try:
+                NoteNews.send_message(Config.LOG_CHANNEL, message)
+                db.update_link(website, link)
+            except FloodWait as e:
+                print(f"FloodWait: {e.x} segundos")
+                sleep(e.x)
+            except Exception as e:
+                print(str(e))
+        else:
+            print(f"FEED Verificado: {link}")
+            
+scheduler = BackgroundScheduler()
+scheduler.add_job(check_send, "interval", seconds=1, max_instances=Config.MAX_INSTANCES)
+scheduler.start()
