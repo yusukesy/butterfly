478da42cd60e1e7e312db39098c53528e198f4ba
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-12 01:47:04 -0300

    update client.py

--- notenews/client.py
+++ notenews/client.py
@@ -13,7 +13,7 @@ class Config:
     
     FEED_URLS = list(set(i for i in os.environ.get("FEED_URLS").split(" | ")))
     # YT_URLS = list(set(i for i in os.environ.get("YT_URLS").split(" | ")))
-    LOG_CHANNEL = str(os.environ.get("LOG_CHANNEL"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     CHECK_INTERVAL = int(os.environ.get("CHECK_INTERVAL"))
     MAX_INSTANCES = int(os.environ.get("MAX_INSTANCES"))
 
