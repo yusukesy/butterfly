da6341c526855d78c87b10c7097ae94af5cf0d43
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-15 18:26:29 -0300

    update news_bs4.py

--- notenews/plugins/news_bs4.py
+++ notenews/plugins/news_bs4.py
@@ -29,7 +29,7 @@ def check_send():
 ▫️ | Mantido por: @NoteZV
 """
             try:
-                NoteNews.send_message(-1001165341477, message)
+                NoteNews.send_message(Config.LOG_CHANNEL, message)
                 db.update_link(website, link)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} segundos")
@@ -40,5 +40,5 @@ def check_send():
             print(f"FEED Verificado: {link}")
             
 scheduler = BackgroundScheduler()
-scheduler.add_job(check_send, "interval", seconds=800, max_instances=Config.MAX_INSTANCES)
+scheduler.add_job(check_send, "interval", seconds=Config.CHECK_INTERVAL, max_instances=Config.MAX_INSTANCES)
 scheduler.start()
