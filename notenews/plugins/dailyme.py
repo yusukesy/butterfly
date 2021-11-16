01f3da63496b640abe4db39b4182abec40877060
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-16 16:31:00 -0300

    update dailyme.py

--- notenews/plugins/dailyme.py
+++ notenews/plugins/dailyme.py
@@ -22,6 +22,10 @@ def check_send():
         title = str(soup.find("div", attrs={"class": "tec--list__item"}).div.a.string)
         link = str(soup.find("div", attrs={"class": "tec--list__item"}).div.a.get("href"))
     if website == "https://canaltech.com.br/ultimas/":
+        link_ = str(soup.section.a.get("href"))
+        if not (link_.startswith("/smartphone") or link_.startswith("/tecnologia") or link_.startswith("/tablet") or link_.startswith("/windows") or link_.startswith("/fone-de-ouvido") or link_.startswith("/casa-conectada") or link_.startswith("/apps") or link_.startswith("/internet") or link_.startswith("/notebook") or link_.startswith("/games")):
+            link = None
+            return
         author = "CanalTech"
         link = "https://canaltech.com.br" + str(soup.section.a.get("href"))
         title = str(soup.section.a.h3.string)
