fdb0d90d58550f3b07bf76c4fbbef80592840562
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-19 14:26:35 -0300

    update dailyme.py

--- notenews/plugins/dailyme.py
+++ notenews/plugins/dailyme.py
@@ -27,7 +27,7 @@ def check_send():
         entry = feed.entries[0]
         title = entry.title
         link_ = entry.link
-        if not (link_.startswith("https://canatech.com.br/smartphone") or link_.startswith("https://canatech.com.br/tecnologia") or link_.startswith("https://canatech.com.br/tablet") or link_.startswith("https://canatech.com.br/windows") or link_.startswith("https://canatech.com.br/fone-de-ouvido") or link_.startswith("https://canatech.com.br/casa-conectada") or link_.startswith("https://canatech.com.br/internet") or link_.startswith("https://canatech.com.br/notebook") or link_.startswith("https://canatech.com.br/software")):
+        if not (link_.startswith("https://canaltech.com.br/smartphone") or link_.startswith("https://canaltech.com.br/tecnologia") or link_.startswith("https://canaltech.com.br/tablet") or link_.startswith("https://canaltech.com.br/windows") or link_.startswith("https://canaltech.com.br/fone-de-ouvido") or link_.startswith("https://canaltech.com.br/casa-conectada") or link_.startswith("https://canaltech.com.br/internet") or link_.startswith("https://canaltech.com.br/notebook") or link_.startswith("https://canaltech.com.br/software")):
             link = None
             return
         link = link_
