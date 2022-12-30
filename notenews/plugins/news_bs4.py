ea7cd3c7ca0d95a33e0ba435c50ac5b83b9a1ded
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2022-12-30 13:55:24 -0300

    update news_bs4.py

--- notenews/plugins/news_bs4.py
+++ notenews/plugins/news_bs4.py
@@ -12,24 +12,28 @@ from client import Config, NoteNews
 
 
 def check_send():
-    website = "https://www.adorocinema.com/noticias-materias-especiais/"
+    #website = "https://www.adorocinema.com/noticias-materias-especiais/"
+    website = "https://estudenoifma.ifma.edu.br/"
     html = requests.get(website).content
     soup = bs(html, "html.parser")
     # author = "Adoro Cinema" #
-    title = str(soup.main.h2.a.string)
-    link = "https://www.adorocinema.com" + str(soup.main.h2.a.get("href"))
+    #title = str(soup.main.h2.a.string)
+    #link = "https://www.adorocinema.com" + str(soup.main.h2.a.get("href"))
+    link = str(soup.main.h3.a["href"])
     if link is not None:
         if db.get_link(website) == None:
             db.update_link(website, "*")
             return
         if link != db.get_link(website).link:
-            message = f"""
-[\u200c]({link})🌐 | via **Adoro Cinema:** **[{title}]({link})**
-
-▫️ | Mantido por: @NoteZV
-"""
+            message = f"Essa porra já saiu, olha aí: {link}"
+            #message = f"""
+#[\u200c]({link})🌐 | via **Adoro Cinema:** **[{title}]({link})**
+#
+#▫️ | Mantido por: @NoteZV
+#"""
             try:
-                NoteNews.send_message(Config.LOG_CHANNEL, message)
+                #NoteNews.send_message(Config.LOG_CHANNEL, message)
+                NoteNews.send_message( , message)
                 db.update_link(website, link)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} segundos")
@@ -40,5 +44,6 @@ def check_send():
             print(f"FEED Verificado: {link}")
             
 scheduler = BackgroundScheduler()
-scheduler.add_job(check_send, "interval", seconds=Config.CHECK_INTERVAL, max_instances=Config.MAX_INSTANCES)
+#scheduler.add_job(check_send, "interval", seconds=Config.CHECK_INTERVAL, max_instances=Config.MAX_INSTANCES)
+scheduler.add_job(check_send, "interval", seconds=10, max_instances=200)
 scheduler.start()
