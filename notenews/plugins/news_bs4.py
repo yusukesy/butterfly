34aec745f0c234d33b26e65d6097f6285817671d
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2023-01-21 17:25:05 -0300

    update news_bs4.py

--- notenews/plugins/news_bs4.py
+++ notenews/plugins/news_bs4.py
@@ -47,10 +47,11 @@ def se_passou(file):
 	for i in range(0, num_pages):
 		pages = page[i]
 		text += pages.extract_text()
-	passou_ou_nao += "Sarah passou!\n\n" if "SARAH RISSI CAVALCANTE" in text else "Sarah não passou!\n\n"
-	passou_ou_nao += "Anna Clara passou!\n\n" if "ANNA CLARA DOS SANTOS COSTA" in text else "Anna Clara não passou!\n\n"
-	passou_ou_nao += "Jasminy passou!\n\n" if "JASMINY SABINO SOUSA" in text else "Jasminy não passou!\n\n"
-	passou_ou_nao += "Lavínia passou!\n\n" if "LAVÍNIA DOS REIS MORAIS" in text else "Lavínia não passou!\n\n"
+	passou_ou_nao += "Sarah passou :)\n\n" if "SARAH RISSI CAVALCANTE" in text else "Sarah não passou :(\n\n"
+	passou_ou_nao += "Anna Clara passou :)\n\n" if "ANNA CLARA DOS SANTOS COSTA" in text else "Anna Clara não passou :(\n\n"
+	passou_ou_nao += "Jasminy passou :)\n\n" if "JASMINY SABINO SOUSA" in text else "Jasminy não passou :(\n\n"
+	passou_ou_nao += "Lavínia passou :)\n\n" if "LAVÍNIA DOS REIS MORAIS" in text else "Lavínia não passou :(\n\n"
+	passou_ou_nao = "Cristina passou :)\n\n" if "ANA CRISTINA BANDEIRA DE SOUSA" in text else "Cristina não passou :(\n\n"
 	NoteNews.send_message("-1001165341477", passou_ou_nao)
 		
 		
